40d5ee35c69d0cefbb01a6d40bc83f62185b2bc2
Rick <Rick@yeah.net>
2018-11-22 15:54:13 +0800

    配置文件

---
+++ Assets/NetworkPractice/Scripts/Config/Config.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class Config : MonoBehaviour {
+	public string jsonPath = null;
+	private string jsonStr = null;
+	private JSONObject jsonRoot = null;
+	public bool Initialized
+	{
+		get
+		{return jsonStr != null;}
+	}
+	private static Config _instance = null;
+	public static Config instance
+	{
+		get
+		{
+			if(_instance==null)
+			{
+				_instance = FindObjectOfType<Config>();
+			}
+			return _instance;
+		}
+	}
+	// Use this for initialization
+	void Awake () {
+		jsonStr = ReadJsonFile(jsonPath);
+		jsonRoot = new JSONObject(jsonStr);
+	}
+
+	public string ReadJsonFile(string path)
+	{
+		if(File.Exists(path))
+		{
+			return File.ReadAllText(path);
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public JSONObject GetJsonObj(string uri)
+	{
+		string[] fields = uri.Split("/".ToCharArray(),StringSplitOptions.RemoveEmptyEntries);
+		JSONObject jo = jsonRoot;
+		foreach(string field in fields)
+		{
+			jo = jo.GetField(field);
+			if(jo == null)
+			{
+				return null;
+			}
+		}
+		return jo;
+	}
+
+	void OnEnable()
+	{
+		if(_instance==null)
+		{
+			_instance = this;
+		}
+		else if(_instance!=this)
+		{
+			Debug.LogWarning("Try to initialize another instance for singleton class Config!");
+		}
+	}
+
+	void OnDestroy()
+	{
+		if(_instance==this)
+		{
+			_instance = null;
+		}
+	}
+}
